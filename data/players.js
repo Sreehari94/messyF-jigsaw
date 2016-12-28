7b7d9379efcdc5507e7ed4baf431f425ad77cecd
sapta <sapta@indisparrow.com>
2016-12-28 20:17:03 +0530

    player data enriched

--- data/players.js
+++ data/players.js
@@ -1,15 +1,148 @@
 var players = {
-	"jwala" : {
-		id : "jwala",
-		name : "JWALA GUTTA",
-		photo : "data/images/jwala-gutta.jpg",
-		team: "Delhi Acers"
-	},
+      "Akshay" : {
+            id : "Akshay",
+            name : "Akshay Dewalkar",
+            photo : "data/images/dAcers/Dewalkar.png",
+            team: "Delhi Acers"
+      },
 
-	"nitchaon" : {
-		id : "nitchaon",
-		name : "NITCHAON JINDAPON",
-		photo : "data/images/nitchaon_jindapol.jpg",
-		team: "Delhi Acers"
-	}
+      "Akshay1" : {
+            id : "Akshay1",
+            name : "Akshay Dewalkar",
+            photo : "data/images/dAcers/Dewalkar-1.jpg",
+            team: "Delhi Acers"
+      },
+
+      "bisht" : {
+            id : "bisht",
+            name : "Madhumita Bisht",
+            photo : "data/images/dAcers/bisht.jpeg",
+            team: "Delhi Acers"
+      },
+
+      "ivan2" : {
+            id : "ivan2",
+            name : "Ivan Sozonov",
+            photo : "data/images/dAcers/sozonov-2.jpg",
+            team: "Delhi Acers"
+      },
+
+      "ivan1" : {
+            id : "ivan1",
+            name : "Ivan Sozonov",
+            photo : "data/images/dAcers/sozonov-1.jpg",
+            team: "Delhi Acers"
+      },
+
+      "Jorgensen1" : {
+            id : "Jorgensen1",
+            name : "Jan O Jorgensen",
+            photo : "data/images/dAcers/Jorgensen-1.jpg",
+            team: "Delhi Acers"
+      },
+
+      "Jorgensen2" : {
+            id : "Jorgensen2",
+            name : "Jan O Jorgensen",
+            photo : "data/images/dAcers/Jorgensen-2.jpg",
+            team: "Delhi Acers"
+      },
+
+      "Jorgensen3" : {
+            id : "Jorgensen3",
+            name : "Jan O Jorgensen",
+            photo : "data/images/dAcers/Jorgensen-3.png",
+            team: "Delhi Acers"
+      },
+
+      "Jwala" : {
+            id : "Jwala",
+            name : "Jwala Gutta",
+            photo : "data/images/dAcers/Jwala-Gutta.png",
+            team: "Delhi Acers"
+      },
+
+      "Jwala1" : {
+            id : "Jwala1",
+            name : "Jwala Gutta",
+            photo : "data/images/dAcers/Jwala-Gutta-1.jpg",
+            team: "Delhi Acers"
+      },
+
+      "Jwala2" : {
+            id : "Jwala2",
+            name : "Jwala Gutta",
+            photo : "data/images/dAcers/Jwala-Gutta-2.jpg",
+            team: "Delhi Acers"
+      },
+
+      "Nichaon1" : {
+            id : "Nichaon1",
+            name : "Nichaon Jindapon",
+            photo : "data/images/dAcers/Nichaon-1.jpg",
+            team: "Delhi Acers"
+
+      },
+
+      "Nichaon2" : {
+            id : "Nichaon2",
+            name : "Nichaon Jindapon",
+            photo : "data/images/dAcers/Nichaon-2.png",
+            team: "Delhi Acers"
+
+      },
+
+      "Nichaon3" : {
+            id : "Nichaon3",
+            name : "Nichaon Jindapon",
+            photo : "data/images/dAcers/Nichaon-3.png",
+            team: "Delhi Acers"
+
+      },
+
+
+      "Siril1" : {
+            id : "Siril1",
+            name : "Siril Verma",
+            photo : "data/images/dAcers/Siril-1.jpg",
+            team: "Delhi Acers"
+      },
+
+      "Siril2" : {
+            id : "Siril2",
+            name : "Siril Verma",
+            photo : "data/images/dAcers/Siril.png",
+            team: "Delhi Acers"
+      },
+
+      "sonwan1" : {
+            id : "sonwan1",
+            name : "Son Wan Ho",
+            photo : "data/images/dAcers/son-wan-1.png",
+            team: "Delhi Acers"
+      },
+
+
+      "sonwan2" : {
+            id : "sonwan2",
+            name : "Son Wan Ho",
+            photo : "data/images/dAcers/son-wan-2.jpg",
+            team: "Delhi Acers"
+      },
+
+
+      "sozonov1" : {
+            id : "sozonov1",
+            name : "Vladimir Ivanov",
+            photo : "data/images/dAcers/sozonov-1.jpg",
+            team: "Delhi Acers"
+      },
+
+
+      "sozonov2" : {
+            id : "sozonov2",
+            name : "Vladimir Ivanov",
+            photo : "data/images/dAcers/sozonov-2.jpg",
+            team: "Delhi Acers"
+      }
 };
